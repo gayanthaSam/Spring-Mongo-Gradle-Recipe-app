f910c3bd1b2e03df351dc942672c30a27094dbed
GayanthaSam <galuthgama@gmail.com>
2024-06-06 20:34:00 +0900

    initial commit

---
+++ src/main/java/gayantha/springframework/spring_recipe_app/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package gayantha.springframework.spring_recipe_app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({ "", "/", "/index" })
+    public String getIndexPage() {
+        return "index";
+    }
+}
