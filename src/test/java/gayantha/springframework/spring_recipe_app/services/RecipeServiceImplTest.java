4f537813a76f2063398eeb06af876dc84f3f5d8c
GayanthaSam <galuthgama@gmail.com>
2024-06-11 02:28:23 +0900

    Unit tests.
    @Mock, when, verify, times

---
+++ src/test/java/gayantha/springframework/spring_recipe_app/services/RecipeServiceImplTest.java
@@ -0,0 +1,48 @@
+package gayantha.springframework.spring_recipe_app.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import gayantha.springframework.spring_recipe_app.domain.Recipe;
+import gayantha.springframework.spring_recipe_app.repositories.RecipeRepository;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void testGetRecipes() {
+
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet<>();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+
+    }
+
+}
