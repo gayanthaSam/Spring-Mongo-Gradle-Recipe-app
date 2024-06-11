7d7d40e7045d6bba529bab1e3fbbdf01280659fb
GayanthaSam <galuthgama@gmail.com>
2024-06-11 23:34:10 +0900

    Unit Tests ArgumentCaptor<T>

---
+++ src/test/java/gayantha/springframework/spring_recipe_app/controllers/IndexControllerTest.java
@@ -0,0 +1,61 @@
+package gayantha.springframework.spring_recipe_app.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import gayantha.springframework.spring_recipe_app.domain.Recipe;
+import gayantha.springframework.spring_recipe_app.services.RecipeService;
+
+public class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController indexController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void testGetIndexPage() {
+
+        // Given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+        when(recipeService.getRecipes()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        // When
+        String viewName = indexController.getIndexPage(model);
+
+        // Then
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
+    }
+}
