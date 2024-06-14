f24ad1f0cebbf199756815e7599b44a88c19496d
GayanthaSam <galuthgama@gmail.com>
2024-06-14 23:26:44 +0900

    fixing the build

--- src/test/java/gayantha/springframework/spring_recipe_app/controllers/RecipeControllerTest.java
+++ src/test/java/gayantha/springframework/spring_recipe_app/controllers/RecipeControllerTest.java
@@ -56,7 +56,7 @@ public class RecipeControllerTest {
     public void testGetNewRecipeForm() throws Exception {
         mockMvc.perform(get("/recipe/new"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("recipe/recipeform"))
+                .andExpect(view().name("/recipe/recipeform"))
                 .andExpect(model().attributeExists("recipe"));
     }
 
